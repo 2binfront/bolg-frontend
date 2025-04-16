03c7d979290e44b12e42aba644373114e2828f58
2binfront <824225714@qq.com>
2025-04-17 00:10:11 +0800

    fix: 🐛 重复页面元素bug尝试修复

--- app.vue
+++ app.vue
@@ -2,23 +2,15 @@
 const userStore = useUserStore();
 const articleStore = useArticleStore();
 onMounted(async () => {
-  await userStore.checkAuth();
-  await articleStore.getAllArticles();
+    await userStore.checkAuth();
+    await articleStore.getAllArticles();
 });
 </script>
 
 <template>
-  <div>
-    <Html lang="en">
-      <Head>
-        <Title>Blog</Title>
-        <Meta name="description" content="A simple blog system with Nuxt3 and Nest" />
-      </Head>
-      <Body>
-        <NuxtLayout>
-          <NuxtPage />
-        </NuxtLayout>
-      </Body>
-    </Html>
-  </div>
+
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
+
 </template>
